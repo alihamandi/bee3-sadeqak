388e87160296675ced89845fb4fefc32415bcce3
Ali Y. Hamandi <aliyasiralasady@hotmail.com>
2019-04-11 14:30:05 +0300

    fixing thinks in the styling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      status: "available",
+      status: "",
       id: "",
       name: "",
       img: "",
